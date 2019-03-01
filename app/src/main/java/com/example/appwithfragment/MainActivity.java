4d1f99f06a1d736d93bfe654b4b82ca0cb98fad4
sherruh <sherruh@gmail.com>
2019-03-01 20:45:55 +0600

    add fragment container

--- app/src/main/java/com/example/appwithfragment/MainActivity.java
+++ app/src/main/java/com/example/appwithfragment/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.appwithfragment;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.appwithfragment.fragments.TextViewFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        TextViewFragment fragment=new TextViewFragment();
+        fragmentTransaction.add(R.id.fragment_container,fragment);
+        fragmentTransaction.commit();
     }
 }
